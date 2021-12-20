60efa86f0cb75349cc6f3b033d6e6666d0e350ac
Piyush Kochhar <piyush.kochhar1@gmail.com>
2021-12-19 18:30:22 -0600

    Updated #8, #9 & #10

--- SQLProject1.sql
+++ SQLProject1.sql
@@ -23,7 +23,7 @@ SELECT * FROM payment INNER JOIN customer ON payment.customer_id=customer.custom
 
 -- 6. Use a View to get the film info for actor Bob Fawcett.
 
-SELECT film_info FROM actor_info WHERE first_name='Bob'AND last_name='Fawcett';
+SELECT * FROM film_list WHERE actors LIKE '%BOB FAWCETT%';
 
 -- 7. Use a Stored Procedure to get the 4 inventory ids for the film "Alien Center" at Store #2. 
 
@@ -32,16 +32,55 @@ CALL `sakila`.`film_in_stock`(@alien_center_film_id, 2, @inventory_id);
 
 -- 8. Insert a new store. Ensure that you use TRANSACTION. (This one is possible but it's tough! Pay attention to constraints and the order that you are inserting data.)
 
+
 START TRANSACTION;
-		INSERT INTO staff(first_name, last_name,address_id,store_id,username) VALUES('Richie','Rich',5,2,'RR');
-        SET @new_staff_id = (SELECT MAX(staff_id) FROM staff);
-		INSERT INTO store(manager_staff_id, address_id) VALUES (@new_staff_id,5); 
+
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, `active`, username) 
+VALUES ("John", "Doe", "18", "rando@gmail.com", "2", "1", "newJohn");
+
+SET @staffId = LAST_INSERT_ID();
+
+INSERT INTO address (address, district, city_id, phone, location) 
+VALUES ('110 Main', 'Dime Box', 300, 7137778888, ST_GeomFromText('POINT(1 1)'));
+
+SET @addressId = LAST_INSERT_ID();
+
+INSERT INTO store (manager_staff_id, address_id) 
+VALUES (@staffId, @addressId);
+
+SET @storeId = LAST_INSERT_ID();
+
+UPDATE staff 
+SET store_id = @storeId
+WHERE staff_id = @staffId;
+
+ROLLBACK;
 
 -- 9. Update the timestamp to the current date/time for the new store you entered in the previous question. 
 
-SET @new_store_id = (SELECT MAX(store_id) FROM store);
-UPDATE store SET last_update = CURRENT_TIMESTAMP WHERE store_id = @new_store_id;
+START TRANSACTION;
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE store AS s
+INNER JOIN address AS a ON a.address_id = s.address_id
+SET s.last_update = CURDATE()
+WHERE a.address = '110 Main';
+
+SET SQL_SAFE_UPDATES = 1;
+
+ROLLBACK;
 
 -- 10. Delete the new store. 
+START TRANSACTION;
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE s.*
+FROM store AS s
+INNER JOIN address AS a ON a.address_id = s.address_id
+WHERE a.address = '110 Main';
+
+SET SQL_SAFE_UPDATES = 1;
 
 ROLLBACK;
