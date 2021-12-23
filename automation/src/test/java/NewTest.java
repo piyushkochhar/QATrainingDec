c7110265527f846d143f88102370705bd3850450
Piyush Kochhar <piyush.kochhar1@gmail.com>
2021-12-22 23:42:48 -0600

    Adds test to get values from config file

--- automation/src/test/java/NewTest.java
+++ automation/src/test/java/NewTest.java
@@ -1,7 +1,22 @@
+
+import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  @Test
-  public void f() {
+  
+
+  @Test()
+  public void canGetBrowserTypeFromFile() throws Exception {
+	  ConfigurationProvider pv = new ConfigurationProvider();
+	  HashMap<String, String> config = pv.getPropertiesFromResourceFile("config.properties");
+	  String actualBrowserTypeValue = "chrome";
+	  
+	  String expectedBrowserTypeValue = config.get("BrowserType");
+	  
+	  Assert.assertEquals(actualBrowserTypeValue, expectedBrowserTypeValue);
   }
+  
 }
