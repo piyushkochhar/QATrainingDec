6f995d44677f27369a4b47d1c78dee48c5e4c9dc
Piyush Kochhar <piyush.kochhar1@gmail.com>
2021-12-28 20:28:12 -0600

    Java Selenium Basics

---
+++ Selenium/src/test/java/SeleniumTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	public WebDriver driver;
+	private final String url = "https://www.selenium.dev/";;
+	
+
+	@Test
+	public void canClickButton() {
+		WebElement element = driver.findElement(By.cssSelector("a[href='/documentation/webdriver/']"));
+		element.click();
+		driver.quit();
+	}
+
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+		this.driver = new ChromeDriver();
+		this.driver.navigate().to(url);
+	}
+	
+	@AfterMethod
+	public void cleanup() {
+		this.driver.quit();
+	}
+}
